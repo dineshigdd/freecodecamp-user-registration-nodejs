300244b15ec0c5196dd674ac6dd5d4db0fdbe489
Glitch (deserted-cardinal) <none>
2019-11-19 07:24:04 +0000

    👩‍👩‍👦🍋 Checkpoint ./server.js:6079345/700 ./public/client.js:6079345/66

--- server.js
+++ server.js
@@ -10,11 +10,13 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine','pug');
-// app.route('/')
-//   .get((req, res) => {
-//     res.sendFile(process.cwd() + '/views/index.html');
-//   });
+
+app.route('/')
+  .get((req, res) => {    
+    res.render(process.cwd() + '/views/pug/index.pug');
+  });
 
 
 
