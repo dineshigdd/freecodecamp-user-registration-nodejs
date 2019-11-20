952aaed109abed9bf35dec6bc6d3a908e1d3a448
Glitch (deserted-cardinal) <none>
2019-11-20 05:51:20 +0000

    🚶🍣 Checkpoint ./server.js:6079345/2325

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
 const ObjectID = require('mongodb').ObjectID;
-const mongo = require('mongodb').MongoClient;
+//const mongo = require('mongodb').MongoClient;
 
 const app = express();
 
@@ -44,17 +44,18 @@ app.route('/')
     res.render(process.cwd() + '/views/pug/index.pug', { title:'Hello' , message:'Please login' });
   });
 
-
-mongo.connect(process.env.DATABASE, (err , db) => {
-  if(err) {
-    console.log('Database error: ' + err);
-  } else {
-    console.log('Successful database connection');
-
-    //serialization and app.listen
-        app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
         console.log("Listening on port " + process.env.PORT);
         });
-  }
+  
 });
+
+// mongo.connect(process.env.DATABASE, (err , db) => {
+//   if(err) {
+//     console.log('Database error: ' + err);
+//   } else {
+//     console.log('Successful database connection');
+
+    //serialization and app.listen
+        
 
