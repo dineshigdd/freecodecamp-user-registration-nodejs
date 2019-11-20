f1b8a251a89c88745dcf9fc1dd4eb3cb20c118d9
Glitch (deserted-cardinal) <none>
2019-11-20 06:03:21 +0000

    ✍️👟 Checkpoint ./server.js:6079345/2137

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
 const ObjectID = require('mongodb').ObjectID;
-//const mongo = require('mongodb').MongoClient;
+const mongo = require('mongodb').MongoClient;
 
 const app = express();
 
@@ -28,34 +28,39 @@ passport.serializeUser((user, done) => {
   done( null, user._id);
 });
 
-passport.deserializeUser((id, done) => {
-  db.collection('users').findOne(
-    {_id: new ObjectID(id)},
-      (err, doc) => {
-        done(null, null);
-      }
-  );
-})
+
 
 app.set('view engine','pug');
 
 app.route('/')
   .get((req, res) => {    
     res.render(process.cwd() + '/views/pug/index.pug', { title:'Hello' , message:'Please login' });
-  });
-
-app.listen(process.env.PORT || 3000, () => {
-        console.log("Listening on port " + process.env.PORT);
-        });
+  });       
   
-});
 
-// mongo.connect(process.env.DATABASE, (err , db) => {
-//   if(err) {
-//     console.log('Database error: ' + err);
-//   } else {
-//     console.log('Successful database connection');
+mongo.connect(process.env.DATABASE, (err, db) => {
+  if(err) {
+    console.log('Database error: ' + err);
+  } else {
+    console.log('Successful database connection');
 
     //serialization and app.listen
+    passport.deserializeUser((id, done) => {
+        db.collection('users').findOne(
+          {_id: new ObjectID(id)},
+            (err, doc) => {
+              done(null, doc);
+            }
+        );
+    })
+    
+       
+   app.listen(process.env.PORT || 3000, () => {
+        console.log("Listening on port " + process.env.PORT);
+     
+   });
+    
+   }
+});
         
 
